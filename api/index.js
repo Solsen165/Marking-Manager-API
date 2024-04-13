c359eb1b8fc438a4770fd877499244cbc3447e42
Solsen165 <islammohsen165@gmail.com>
2024-04-14 00:38:38 +0800

    Enabling urlencoded and json

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ const dao = require('./dataAccess');
 const models = require('./classes')
 
 app.use(cors());
-//app.use(express.json());
-//app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
